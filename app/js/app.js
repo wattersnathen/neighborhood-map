366e3bedecad50d2386f5e28f062764bc8ba5e01
Nathen Watters <wattersnathen@gmail.com>
2015-05-06 19:39:57 -0600

    loading gif on AJAX requests

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,18 @@
     'use strict';
 
     $(document).ready(function() {
+
+        $(document).ajaxStart(function() {
+            $("#loading").show();
+        });
+
+        $(document).ajaxStop(function() {
+            $("#loading").hide();
+        });
+
         var map = new Map();
         map.getCurrentPosition();
         ko.applyBindings(map.viewModel);
     });
 
-
-})();
+})(); 
