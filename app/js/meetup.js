aaad22626e1b119c1882eb1451222fca8e47d870
Nathen Watters <wattersnathen@gmail.com>
2015-05-10 17:15:38 -0600

    added comments, updated to change marker icons based on how far away the meetup event is from now

--- app/js/meetup.js
+++ app/js/meetup.js
@@ -1,9 +1,19 @@
+var today = moment(Date.now()); // today's Date
+
+// Meetup object for dealing with the Meetup JavaScript API
 var meetup = {
     key: '82d672183c733619762e644e64e2c',
     viewModel: {
         meetups: ko.observableArray([]),
         markers: ko.observableArray([])
     },
+
+    /* 
+     * Get the Meetups that have an upcoming event date.
+     * @param {Number} rad the radius to search from
+     * @param {Number} zipCode the postal code to search Meetups in
+     * @param {Object} map a reference to the Google Maps object
+     */
     getUpcomingMeetups: function(rad, zipCode, map) {
         var url = 'https://api.meetup.com/2/open_events?sign=true&photo-host=public&status=upcoming&zip=' + zipCode + '&radius=' + rad + '&key=' + meetup.key;
         $.ajax({
@@ -25,16 +35,46 @@ var meetup = {
             }
         });
     },
+
+    /*
+     * Create a Google Maps Marker to place on the map
+     * @param {Object} value Meetup result returned from the API 
+     * @param {Object} map Reference to the Google Map used
+     */
     createMarker: function(value, map) {
         var pos = new google.maps.LatLng(value.venue.lat, value.venue.lon);
-        var marker = new google.maps.Marker({
-            position: pos,
-            map: map
-        });
-
         var date = moment(value.time).toDate();
+        var icon;
+
+        var isToday = moment(date).isSame(today, 'day');
+
+        if (isToday) {
+            // green icon for today's Meetups
+            icon = 'icons/today1.png';
+        } else if (moment(date).subtract(7, 'days') <= moment(today)) {
+            // blue icon for this week's Meetups
+            icon = 'icons/marker-th.png';    
+        } else {
+            // red icon for all other upcoming Meetups
+            icon = 'icons/map-pin-red-th.png';
+        }
+
+        // reformat the date for easier display in the markup
         date = moment(date).format('MMMM Do YYYY, h:mm a');
 
+        // marker object to place on the map
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            icon: icon
+        });
+
+        if (isToday) {
+            // bounce the marker if the Meetup is today
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+        }
+
+        // InfoWindow content template
         var content = 
             '<div class="infowindow">' +
                 '<a href="' + value.event_url +'" target="_blank" class="eventurl">' +
@@ -48,6 +88,7 @@ var meetup = {
                 '<p>' + value.description + '</p>' +
             '</div>';
 
+        // give each marker an InfoWindow
         marker.info = new google.maps.InfoWindow({
             content: content
         });
@@ -56,11 +97,28 @@ var meetup = {
 
         marker.date = date;
 
+        marker.open = false;    // tracking the InfoWindow's status of either open or closed
+
+        // user closes the InfoWindow
+        google.maps.event.addListener(marker.info, 'closeclick', function() {
+            marker.open = false;
+        });
+
+        // open/close the InfoWindow when marker is clicked
         google.maps.event.addListener(marker, 'click', function() {
             for (var i in meetup.viewModel.markers()) {
-                meetup.viewModel.markers()[i].info.close();
+                if (meetup.viewModel.markers()[i].id != marker.id) {
+                    meetup.viewModel.markers()[i].open = false;
+                    meetup.viewModel.markers()[i].info.close();
+                }
             }
-            marker.info.open(map, marker);
+            if (marker.open) {
+                marker.open = false;
+                marker.info.close();
+            } else {
+                marker.open = true;
+                marker.info.open(map, marker);
+            }
         });
 
         return marker;
