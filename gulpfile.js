ba2c3031b372d35a5d2ee7b51eb4151198d161ba
Nathen Watters <wattersnathen@gmail.com>
2015-05-10 18:23:08 -0600

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var usemin = require('gulp-usemin');
+var uglify = require('gulp-uglify');
+var minifyHtml = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var rev = require('gulp-rev');
+
+gulp.task('clean', function() {
+    return gulp.src('build/', {read:false})
+        .pipe(clean());
+});
+
+gulp.task('usemin', function() {    
+    return gulp.src('./app/*.html')
+        .pipe(usemin({
+            css: [minifyCss()],
+            html: [minifyHtml({empty: true})],
+            js: [uglify(), rev()],
+            js1: [uglify()]
+        }))
+        .pipe(gulp.dest('build/'));
+});
+
+gulp.task('movefiles', function() {
+    gulp.src('./app/icons/*.png')
+        .pipe(gulp.dest('build/icons'));
+});
+
+gulp.task('default', ['usemin', 'movefiles']);
