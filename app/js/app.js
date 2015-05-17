956db6bbb29aa31c631e39343b685c91d230cdf8
Nathen Watters <wattersnathen@gmail.com>
2015-05-16 20:30:06 -0600

    update

--- app/js/app.js
+++ app/js/app.js
@@ -1,29 +1,30 @@
-(function() {
+(function ($) {
     'use strict';
 
-    $(document).ready(function() {
-
+    var map = new Map();
+    
+    $(document).ready(function () {
+        
         // Show loading gif during AJAX requests
-        $(document).ajaxStart(function() {
+        $(document).ajaxStart(function () {
             $(".results").text('');
             $(".loading").show();
         });
 
-        $(document).ajaxStop(function() {
+        $(document).ajaxStop(function () {
             $(".loading").hide();
             // display a message to the user if no meetups for the area were found
             if (map.viewModel.meetups()[0] === undefined) {
                 $(".results").text('It appears that there are no upcoming Meetups in the chosen area. Please search for a new location or try again later.');
             }
         });
-
-        var map = new Map();
-        map.getCurrentPosition();
-        ko.applyBindings(map.viewModel);
+        
+        ko.applyBindings(map.viewModel);        
+        map.setDefaultPosition();
         
         $("#toggle-menu").on('click', function (e) {
             $(".container").toggle();
         });
     });
 
-})();
+})(jQuery);
